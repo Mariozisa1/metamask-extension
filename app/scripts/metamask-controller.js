a128c36885fc33868e0a3ab90b8132dd4699f199
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-10-21 13:17:08 -0700

    chore: fetch quotes when account has insufficient balance

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2111,7 +2111,10 @@ export default class MetamaskController extends EventEmitter {
 
     const bridgeControllerMessenger = this.controllerMessenger.getRestricted({
       name: BRIDGE_CONTROLLER_NAME,
-      allowedActions: ['AccountsController:getSelectedAccount'],
+      allowedActions: [
+        'AccountsController:getSelectedAccount',
+        'NetworkController:getSelectedNetworkClient',
+      ],
       allowedEvents: [],
     });
     this.bridgeController = new BridgeController({
